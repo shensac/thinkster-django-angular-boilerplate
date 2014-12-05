51d8c43c9752e5e253504243ade1b0fa3533b505
Sachin Shende <sachin.shende@kisanhub.com>
2014-12-05 19:56:00 +0000

    SS: Added api endpoints and js structure

---
+++ thinkster_django_angular_boilerplate/authentication/views.py
@@ -0,0 +1,36 @@
+from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+
+from thinkster_django_angular_boilerplate.authentication.models import Account
+from thinkster_django_angular_boilerplate.authentication.permissions import IsAccountOwner
+from thinkster_django_angular_boilerplate.authentication.serializers import AccountSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.DATA)
+
+        if serializer.is_valid():
+            account = Account.objects.create_user(**request.DATA)
+
+            account.set_password(request.DATA.get('password'))
+            account.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({
+                            'status': 'Bad request',
+                            'message': 'Account could not be created with received data.'
+                        }, status=status.HTTP_400_BAD_REQUEST)
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,17 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from django.views.generic import TemplateView
+from rest_framework_nested import routers
 
 from thinkster_django_angular_boilerplate.views import IndexView
+from thinkster_django_angular_boilerplate.authentication.views import \
+    AccountViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
-
-    url('^.*$', IndexView.as_view(), name='index'),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 )
